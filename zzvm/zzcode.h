3dc5e2c257933574aa9827de9932c4e5cc2064eb
Inndy <inndy.tw@gmail.com>
2016-07-08 17:43:13 +0800

    Format zzcode.h for python library

--- zzvm/zzcode.h
+++ zzvm/zzcode.h
@@ -1,33 +1,49 @@
+/*
+ * NOTICE THAT YOU CAN NOT MODIFY THIS FILE, PYTHON LIB WILL PARSE THIS FILE!!
+ *
+ * You must add ',' to every line.
+ * You muse add the comment at every line.
+ *
+ * Comment are composed in two parts: `T` and `C`
+ *
+ * `T` could be character list below
+ *
+ *     - I means I-type instruction
+ *     - R means R-type instruction
+ *
+ * `C` means how many registers are used in this instruciton
+ */
+
 enum ZZOP {
-    ZZOP_NOP    = 0x00,
-    ZZOP_NEG    = 0x01,
-    ZZOP_ADDR   = 0x02,
-    ZZOP_ADDI   = 0x03,
-    ZZOP_MULR   = 0x04,
-    ZZOP_MULI   = 0x05,
-    ZZOP_ANDR   = 0x06,
-    ZZOP_ANDI   = 0x07,
-    ZZOP_ORR    = 0x08,
-    ZZOP_ORI    = 0x09,
-    ZZOP_XORR   = 0x0a,
-    ZZOP_XORI   = 0x0b,
-    ZZOP_SHRR   = 0x0c,
-    ZZOP_SHRI   = 0x0d,
-    ZZOP_NOT    = 0x0e,
-    ZZOP_LD     = 0x0f,
-    ZZOP_ST     = 0x10,
-    ZZOP_HLT    = 0x11,
-    ZZOP_MOVR   = 0x12,
-    ZZOP_MOVI   = 0x13,
-    ZZOP_JEI    = 0x14,
-    ZZOP_JNI    = 0x15,
-    ZZOP_JGI    = 0x16,
-    ZZOP_JZI    = 0x17,
-    ZZOP_CALL   = 0x18,
-    ZZOP_RET    = 0x19,
-    ZZOP_POP    = 0x1a,
-    ZZOP_PUSH   = 0x1b,
-    ZZOP_PUSI   = 0x1c,
-    ZZOP_SYS    = 0x1d,
-    ZZOP_RAND   = 0x1e,
+    ZZOP_NOP    = 0x00, // R 0
+    ZZOP_NEG    = 0x01, // R 2
+    ZZOP_ADDR   = 0x02, // R 3
+    ZZOP_ADDI   = 0x03, // I 2
+    ZZOP_MULR   = 0x04, // R 3
+    ZZOP_MULI   = 0x05, // I 2
+    ZZOP_ANDR   = 0x06, // R 3
+    ZZOP_ANDI   = 0x07, // I 2
+    ZZOP_ORR    = 0x08, // R 3
+    ZZOP_ORI    = 0x09, // I 2
+    ZZOP_XORR   = 0x0a, // R 3
+    ZZOP_XORI   = 0x0b, // I 2
+    ZZOP_SHRR   = 0x0c, // R 3
+    ZZOP_SHRI   = 0x0d, // I 2
+    ZZOP_NOT    = 0x0e, // R 0
+    ZZOP_LD     = 0x0f, // I 2
+    ZZOP_ST     = 0x10, // I 2
+    ZZOP_HLT    = 0x11, // R 0
+    ZZOP_MOVR   = 0x12, // R 2
+    ZZOP_MOVI   = 0x13, // I 1
+    ZZOP_JEI    = 0x14, // I 2
+    ZZOP_JNI    = 0x15, // I 2
+    ZZOP_JGI    = 0x16, // I 2
+    ZZOP_JZI    = 0x17, // I 1
+    ZZOP_CALL   = 0x18, // I 0
+    ZZOP_RET    = 0x19, // R 0
+    ZZOP_POP    = 0x1a, // R 1
+    ZZOP_PUSH   = 0x1b, // R 1
+    ZZOP_PUSI   = 0x1c, // I 0
+    ZZOP_SYS    = 0x1d, // R 0
+    ZZOP_RAND   = 0x1e, // R 0
 };
