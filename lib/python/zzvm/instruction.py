ba4d3a357be94617a8a59c740e2e822ba3c4fb11
Inndy <inndy.tw@gmail.com>
2017-02-22 11:45:43 +0800

    Add docstring for Instruction object

--- lib/python/zzvm/instruction.py
+++ lib/python/zzvm/instruction.py
@@ -4,6 +4,11 @@ import struct
 
 class Instruction(object):
     def __init__(self, opcode, reg1=None, reg2=None, reg3=None, imm=None):
+        """
+        opcode		type(opcode) in (int, str, Opcode)
+        registers	type(reg) in (int, str, Register)
+        imm		type(imm) is int
+        """
         self.opcode = opcode
         self.reg1 = reg1
         self.reg2 = reg2
@@ -11,17 +16,16 @@ class Instruction(object):
         self.imm = imm
 
     def compose(self):
+        """
+        return encoded instruction in bytes
+        """
         op = Opcodes.normalize(self.opcode)
         if not op:
             raise ValueError('`opcode` is not opcode (%r)' % self.opcode)
 
-        regs = []
-        for i in range(1, 1 + op.regs):
-            reg = Registers.normalize(getattr(self, 'reg%d' % i))
-            if not reg:
-                raise ValueError('`reg%d` is missing' % i)
-            regs.append(reg.code)
-        regs += [0] * (3 - len(regs))
+        regs = map(Registers.normalize, (self.reg1, self.reg2, self.reg3))
+        if not all(regs[i] for i in range(op.regs)):
+            raise ValueError('required for this instruction is missing')
 
         if op.type_ == 'I':
             imm = self.imm & 0xffff
